037336ec3b6e58249a0d0dc5b45cfa4d9164529b
uttoniWork <uttoni.brandani@ges.inatel.br>
2023-10-06 22:23:48 -0300

    Finalizando entidades

--- src/main/java/br/inatel/labs/labjpa/entity/Produto.java
+++ src/main/java/br/inatel/labs/labjpa/entity/Produto.java
@@ -1,34 +1,78 @@
 package br.inatel.labs.labjpa.entity;
 
-import jakarta.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
-import java.util.List;
 
 @Entity
 public class Produto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotNull
-    @Size(min = 2, max = 100)
+    @NotNull @Size(min = 2, max = 100)
     private String descricao;
-
     @ManyToMany(mappedBy = "listaProduto")
     private List<Fornecedor> listaFornecedor;
 
+    public Produto() {
+
+    }
+
+    public Produto(@NotNull @Size(min = 2, max = 100) String descricao) {
+        super();
+        this.descricao = descricao;
+    }
+
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getDescricao() {
         return descricao;
     }
 
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
     public List<Fornecedor> getListaFornecedor() {
         return listaFornecedor;
+    }
+
+    public void setListaFornecedor(List<Fornecedor> listaFornecedor) {
+        this.listaFornecedor = listaFornecedor;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto [id=" + id + ", descricao=" + descricao + "]";
     }
 }
